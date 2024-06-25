ae380f0781ebd49b88535fa287f3a0ee8e50daec
AbdalruhmanIssa <Aboody9068@gmail.com>
2024-06-25 15:37:08 +0300

    exchange

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-var data="";
-var arr=["c++","javascript","python"];
-for(var i= 0;i <3; i++){
-  data+=`<li>${arr[i]}</li>`;
-} 
-document.querySelector("ul").innerHTML=data;
+var exchangeForm = document.querySelector(".exchangeForm");
+var users = [];
+exchangeForm.onsubmit = function (e) {
+  e.preventDefault();
+  var elements = e.target.elements;
+  var user = {
+    element: elements[0].value,
+  };
+  users.push(user);
+  printData();
+};
+function printData() {
+  var currencySelect = document.querySelector(".currencySelect");
+  var selectedCurrency = currencySelect.value;
+  if (selectedCurrency === "dollar") {
+    console.log(users[0].element * 3.5);
+  }
+ else if (selectedCurrency === "dinar") {
+    console.log(users[0].element * 5);
+  }
+  else if (selectedCurrency === "nis") {
+    console.log(users[0].element);
+  }
+}
+console.log(users);
