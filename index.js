9e974b563a3340824cf0ba62f4df096350e14489
AbdalruhmanIssa <Aboody9068@gmail.com>
2024-07-02 00:45:11 +0300

    pizza

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-var exchangeForm = document.querySelector(".exchangeForm");
-var users = [];
-exchangeForm.onsubmit = function (e) {
-  e.preventDefault();
-  var elements = e.target.elements;
-  var user = {
-    element: elements[0].value,
-  };
-  users.push(user);
-  printData();
-};
-function printData() {
-  var currencySelect = document.querySelector(".currencySelect");
-  var selectedCurrency = currencySelect.value;
-  if (selectedCurrency === "dollar") {
-document.querySelector('p').textContent=`from Dollar to nis is:${users[0].element * 3.5}`;
-  }
- else if (selectedCurrency === "dinar") {
-    console.log(users[0].element * 5);
-document.querySelector('p').textContent=`from Dinar to nis is:${users[0].element * 5}`;
-  }
-  else if (selectedCurrency === "nis") {
-    document.querySelector('p').textContent=`it still the same at:${users[0].element }`;
-
-  }
-}
+async function getpizza(){
+  const response=await fetch(`https://forkify-api.herokuapp.com/api/search?q=pizza`)
+const data= await response.json();
+const pizza=data.recipes;
+const result= pizza.map(function(ele){
+return `
+<div class='pizza'>
+<h2>${ele.title}</h2>
+<img src='${ele.image_url}'>
+</div>
+`;
+}).join('');
+document.querySelector(".pizza").innerHTML=result;}
+getpizza();
